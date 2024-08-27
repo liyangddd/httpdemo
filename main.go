c13a1166365412ca2cbef99791490b3772f463a5
gufei <gufei.hrududu@bytedance.com>
2024-08-27 19:45:57 +0800

    add log

--- main.go
+++ main.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"time"
 )
+
+func timer_log(interval int) {
+	for {
+		time.Sleep(time.Duration(interval) * time.Second)
+		log.Printf("Just tell you I'm still alive")
+	}
+}
 
 func get_randam(max int) int {
 	return rand.Intn(max)
 }
 
 func randam(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "%d\n", get_randam(100))
+	r := get_randam(100)
+	log.Printf("%s access /random, return %d", req.RemoteAddr, r)
+	fmt.Fprintf(w, "%d\n", r)
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	log.Printf("%s access /hello", req.RemoteAddr)
 	fmt.Fprintf(w, "hello\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
+	log.Printf("%s access /headers", req.RemoteAddr)
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
@@ -27,7 +41,15 @@ func headers(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-
+	f, err := os.OpenFile("http.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		log.Fatalln("open log file fail")
+	}
+	defer func() {
+		f.Close()
+	}()
+	log.SetOutput(f)
+	go timer_log(10)
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/random", randam)
